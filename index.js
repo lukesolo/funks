9fd3294e4995aa1574da9c2fec8291fdb3c279b7
lukesolo <suddenmadness@yandex.ru>
2017-04-16 17:40:34 +0700

    External handlers

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const {lift, service, sync, or, onFail, _register} = require('./lib/builder');
+const {lift, service, sync, or, onFail, ext, _register} = require('./lib/builder');
 const plan = require('./lib/plan');
 
 module.exports = {
     plan,
-    builder: {lift, service, sync, or, onFail},
+    builder: {lift, service, sync, or, onFail, ext},
     register: _register,
 };
